5ce988aa98654d00183446e21a41bbe183fbd78f
Bennett <110846042+bennett-sh@users.noreply.github.com>
2022-11-02 00:58:04 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,226 @@
+use colored::Colorize;
+use simpleargs::SimpleArgs;
+use std::{
+    cmp::Ordering,
+    env,
+    fs::{self, DirEntry},
+    io::ErrorKind::NotFound,
+    path::PathBuf,
+    process::{exit, Command},
+};
+use version_compare::{compare as compare_versions, Cmp};
+use windirs::{known_folder_path, FolderId};
+
+fn get_wemod_folder() -> PathBuf {
+    let local_app_data =
+        known_folder_path(FolderId::LocalAppData).expect("Local app data could not be found.");
+
+    return local_app_data.join("WeMod");
+}
+
+fn get_version_from_dir_entry(entry: &DirEntry) -> String {
+    return entry.file_name().to_str().unwrap().replacen("app-", "", 1);
+}
+
+fn get_version_from_path(path: PathBuf) -> String {
+    return path
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .replacen("app-", "", 0);
+}
+
+fn sort_app_versions(a: &DirEntry, b: &DirEntry) -> Ordering {
+    return match compare_versions(
+        get_version_from_dir_entry(a.clone()),
+        get_version_from_dir_entry(b.clone()),
+    )
+    .unwrap_or(Cmp::Eq)
+    {
+        Cmp::Gt => Ordering::Greater,
+        Cmp::Lt => Ordering::Less,
+        _ => Ordering::Equal,
+    };
+}
+
+fn run_asar(prog: PathBuf, dir: PathBuf, args: Vec<String>) {
+    let cmd = Command::new(prog.to_str().unwrap())
+        .current_dir(dir)
+        .args(args)
+        .spawn();
+
+    if cmd.is_err() {
+        if let NotFound = cmd.unwrap_err().kind() {
+            err(
+                "asar is not installed. you can install it using 'npm i -g @electron/asar'"
+                    .to_string(),
+            )
+        } else {
+            err("failed run asar command".to_string())
+        }
+    } else {
+        cmd.unwrap().wait().unwrap();
+    }
+}
+
+fn get_latest_app_dir(wemod_dir: PathBuf) -> std::io::Result<PathBuf> {
+    let mut versions = fs::read_dir(wemod_dir)?
+        .map(|result| result.expect("failed to get wemod folder content"))
+        .filter(|entry| {
+            entry.metadata().expect("failed to get metadata").is_dir()
+                && entry
+                    .file_name()
+                    .to_str()
+                    .expect("failed to get folder name")
+                    .starts_with("app-")
+        })
+        .collect::<Vec<DirEntry>>();
+
+    versions.sort_by(|a, b| sort_app_versions(a, b));
+
+    Ok(versions
+        .last()
+        .expect(
+            "failed to find latest WeMod version. you can manually specify it with --wemod-version",
+        )
+        .path())
+}
+
+fn patch(extracted_resource_dir: PathBuf) -> std::io::Result<()> {
+    let app_bundle = extracted_resource_dir.join("output").join("app-bundle.js");
+
+    if !app_bundle.exists() || !app_bundle.is_file() {
+        err("app-bundle.js not found. your WeMod version may not be supported.".to_string());
+    }
+
+    println!("Patching app bundle.");
+
+    let mut app_bundle_contents =
+        fs::read_to_string(&app_bundle).expect("failed to read app bundle");
+
+    let app_bundle_patch = "if(new URL(e.url).pathname.endsWith('/account')){return{...JSON.parse(t),subscription: 'pro'}}";
+    let insert_app_bundle_patch_at =
+        "if(e.headers.get(\"Content-Type\")===\"application/json\"){try{";
+
+    app_bundle_contents.insert_str(
+        app_bundle_contents
+            .find(insert_app_bundle_patch_at)
+            .expect("failed to patch app bundle. WeMod may have changed their program")
+            + insert_app_bundle_patch_at.len(),
+        app_bundle_patch,
+    );
+
+    fs::write(&app_bundle, app_bundle_contents)?;
+
+    println!("Done.");
+
+    Ok(())
+}
+
+fn err(msg: String) {
+    println!("{}", msg.red());
+    exit(1);
+}
+
+fn main() -> std::io::Result<()> {
+    if env::consts::OS != "windows" {
+        err(format!("Your OS ({}) is not supported.", env::consts::OS))
+    }
+
+    let (_, _, opts) = SimpleArgs::new(env::args().collect()).parse();
+
+    let wemod_folder = if opts.contains_key("wemod-dir") {
+        PathBuf::from(opts.get("wemod-dir").unwrap())
+    } else {
+        get_wemod_folder()
+    };
+
+    if !wemod_folder.exists() {
+        err(
+            "WeMod is not installed/not found (specify custom install location with --wemod-dir)."
+                .to_string(),
+        );
+    }
+
+    let wemod_version_folder = if opts.contains_key("wemod-version") {
+        let folder = PathBuf::from(opts.get("wemod-version").unwrap());
+
+        if !folder.exists() {
+            err("WeMod version specified does not exist.".to_string())
+        }
+
+        wemod_folder.join(folder)
+    } else {
+        get_latest_app_dir(wemod_folder).expect(
+            "failed to find latest WeMod version. you can manually specify it with --wemod-version",
+        )
+    };
+
+    let resource_dir = wemod_version_folder.join("resources");
+
+    let asar_bin = if opts.contains_key("asar-bin") {
+        let file = PathBuf::from(opts.get("asar-bin").unwrap());
+
+        if !file.exists() {
+            err("asar binary specified does not exist.".to_string());
+        }
+
+        file
+    } else {
+        let asar = known_folder_path(FolderId::ProgramFiles)
+            .unwrap()
+            .join("nodejs")
+            .join("asar.cmd");
+
+        if !asar.exists() || !asar.is_file() {
+            err("asar not found. you can manually specify it with --asar-bin".to_string())
+        }
+
+        asar
+    };
+
+    println!(
+        "Attempting to patch WeMod version {}...",
+        get_version_from_path(wemod_version_folder)
+    );
+
+    println!("Extracting resources...");
+
+    run_asar(
+        asar_bin.clone(),
+        resource_dir.clone(),
+        vec![
+            "extract".to_string(),
+            "app.asar".to_string(),
+            "app".to_string(),
+        ],
+    );
+
+    println!("Done.");
+
+    let extracted_resource_dir = resource_dir.join("app");
+
+    patch(extracted_resource_dir.clone())?;
+
+    println!("Repacking resources...");
+
+    run_asar(
+        asar_bin.clone(),
+        resource_dir,
+        vec![
+            "pack".to_string(),
+            "app".to_string(),
+            "app.asar".to_string(),
+        ],
+    );
+
+    println!("Done.");
+    println!("Cleaning up...");
+
+    fs::remove_dir_all(extracted_resource_dir).expect("failed to remove extracted resources.");
+
+    println!("Done.");
+
+    Ok(())
+}
