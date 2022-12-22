e26f90ba8c64359d37f41b04ae79cfef4e1748ba
Bennett <110846042+bennett-sh@users.noreply.github.com>
2022-12-22 11:34:09 +0100

    Update vendorPatch.js

--- cli/src/vendorPatch.js
+++ cli/src/vendorPatch.js
@@ -4,7 +4,6 @@
   let trig = () => {
     document.querySelectorAll('promotion-banner').forEach(element => element.remove());
     document.querySelectorAll('remote-button').forEach(element => element.remove());
-    // document.querySelectorAll('.cheats-wrapper button.pro-upgrade').forEach(element => element.remove());
   };
 
   setInterval(() => trig(), 1000);
